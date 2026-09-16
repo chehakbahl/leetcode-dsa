11a92604ff1d11880a3e41ed2d859562f4e95baf
Chehak Bahl <chehak.bahl@gmail.com>
2026-09-17 02:08:58 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.83%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int h=nums.size()-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[l]<=nums[mid]){//left sorted
+                if(target<nums[mid] && target>=nums[l]){
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{//right sorted
+                if(target>nums[mid] && target<=nums[h]){
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
